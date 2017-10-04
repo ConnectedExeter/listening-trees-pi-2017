e0c368f90cf490a06a1aebe5b120f86ccdd68ff5
Michael Saunby <mike@saunby.net>
2017-10-04 16:52:42 +0100

    .

--- set-up.sh
+++ set-up.sh
@@ -1,8 +1,12 @@
+sudo apt-get update
+sudo apt-get -y upgrade
 sudo apt-get -y install sox
 sudo install config-files/etc-asound.conf /etc/asound.conf
 
-sudo apt-get install dnsutils
+sudo apt-get -y install dnsutils
 
 sudo install config-files/etc-rc.local /etc/rc.local
 install listening-trees-auto.sh ~/listening-trees-auto.sh
+
+sudo apt-get -y install xvfb
 
