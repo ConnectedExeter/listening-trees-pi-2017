a210f06657a4c78fce27ff0db7e4cf02d5829253
Michael Saunby <mike@saunby.net>
2017-10-06 15:18:25 +0100

    .

--- set-up.sh
+++ set-up.sh
@@ -8,5 +8,7 @@ sudo apt-get -y install dnsutils
 sudo install config-files/etc-rc.local /etc/rc.local
 install listening-trees-auto.sh ~/listening-trees-auto.sh
 
+sudo install config-files/var-lib-alsa-asaound.state /var/lib/alsa/asound.state
+
 sudo apt-get -y install xvfb
 
