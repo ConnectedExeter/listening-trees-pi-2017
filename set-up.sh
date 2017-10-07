f9f90434ff7ad12597fac5f2afc3d6df3268109b
Michael Saunby <mike@saunby.net>
2017-10-07 14:14:30 +0100

    .

--- set-up.sh
+++ set-up.sh
@@ -1,7 +1,9 @@
 sudo apt-get update
 sudo apt-get -y upgrade
+sudo apt-get -y install pulseaudio 
+sudo apt-get -y install pavucontrol
 sudo apt-get -y install sox
-sudo install config-files/etc-asound.conf /etc/asound.conf
+#sudo install config-files/etc-asound.conf /etc/asound.conf
 
 sudo apt-get -y install dnsutils
 
@@ -11,4 +13,3 @@ install listening-trees-auto.sh ~/listening-trees-auto.sh
 sudo install config-files/var-lib-alsa-asaound.state /var/lib/alsa/asound.state
 
 sudo apt-get -y install xvfb
-
